9643f6621940735478a574b91fc89956f76136ee
Steven Buhrman <scbuhrma@gmail.com>
2012-02-08 10:22:39 -0500

    initial commit of lab 3

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -2,14 +2,37 @@
  * This is the entry point for our JavaScript program
  */
 function main() {
-    //your code goes here
-    alert("hello world!");
+    
+    //alert("hello world!");
 
 
     //your tasks
 
     //1. Create a spotter and get it to insert tweets into the DOM
-    //2. Add profile images (tweet.profile_image_url)
+     var s  = new Spotter("twitter.search",
+	{q:"Bieber",period:120},
+	{buffer:true,bufferTimeOut:750});
+    
+	var count = true;
+	var twitCount = 1;
+    s.register(function(tweet) {
+    	
+        var profile_image = "<img src=\"" + tweet.profile_image_url+ "\" />";
+	if(twitCount==10){
+	    $("#tweets p:last-child").remove();
+            twitCount=1;
+	}
+	if(count){
+	$("#tweets").prepend($("<p class='green'>"+profile_image+tweet.text+"</p>").fadeIn());
+	}else{
+	$("#tweets").prepend($("<p class='teal'>"+profile_image+tweet.text+"</p>").fadeIn());
+	}
+        count = !count;	 	
+    	twitCount++;
+});    
+
+
+	s.start();
     //3. Make the tweets occur so the most recent are at the top
     //4. Make the tweets slide down
     //5. Alternate the colors or the background of the tweets
